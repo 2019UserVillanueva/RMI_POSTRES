49e4ed55b0f0186c72d69b98adbfd6ee539b5182
2019UserVillanueva <al049738@uacam.nx>
2020-04-25 00:53:56 -0500

    Estructura Inicial JPA RMI Ya están creados los métodos CRUD

---
+++ src/main/java/conexion_JPA/conexion.java
@@ -0,0 +1,13 @@
+
+
+package conexion_JPA;
+
+
+public class conexion {
+    
+    public static void main(String[] args) {
+        Inicio i = new Inicio();
+        i.setVisible(true);
+    }
+
+}
